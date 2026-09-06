3e7e592f9e218faecb31251532056a0901066389
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-06 22:34:52 +0300

    Add project files.

---
+++ FourthAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Book
+{
+    private string password = "secret";
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Book book = new Book();
+
+        Console.WriteLine(book.password);
+        // will not compile because password is private and cannot be accessed from outside the Book class
+    }
+}
