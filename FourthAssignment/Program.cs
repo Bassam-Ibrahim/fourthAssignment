d8e55f91d5e501fa820faff4e550165635133746
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-06 22:38:24 +0300

    second question

--- FourthAssignment/Program.cs
+++ FourthAssignment/Program.cs
@@ -2,6 +2,8 @@
 class Book
 {
     private string password = "secret";
+    internal int copiesInStock = 5;
+
 }
 
 class Program
@@ -13,6 +15,10 @@ class Program
         #region first question
         Console.WriteLine(book.password);
         // will not compile because password is private and cannot be accessed from outside the Book class
+        #endregion
+        #region second question
+        Console.WriteLine(book.copiesInStock);
+        //will compile because it is  internal and can be accessed from the same poject
         #endregion
     }
 }
